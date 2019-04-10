2cf38b7c1806432e6340806ce184ba3cab959658
JackFei <1712776213@qq.com>
2019-04-10 23:46:14 +0800

    chore: 删减不必要文件

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import './styles/main.css';
 import 'antd/dist/antd.css';
 import Root from './root/Root';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Root/>, document.getElementById('root'));
-registerServiceWorker();
